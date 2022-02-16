43ce120eea01f74e63fc983cf9b5f200c6b1eaac
Klein Suarez <yahir.kleins@gmail.com>
2022-02-16 17:59:02 -0500

    update events

--- drumKit.js
+++ drumKit.js
@@ -5,8 +5,8 @@ function removeTransition(e) {
 }
 
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
@@ -18,3 +18,16 @@ const keys = document.querySelectorAll('.keys');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
 
+// for mobile version 
+window.addEventListener('click', e => {
+   const keyCode = e.target.getAttribute('data-key');
+
+   const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+   const key = e.target
+
+   if (!audio) return;
+
+   audio.currentTime = 0;
+   audio.play();
+   key.classList.add('playing');
+});
